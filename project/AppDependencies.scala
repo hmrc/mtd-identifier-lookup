0cbee84a3dd13c3d917b344cf09c143be2bc58ee
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2018-06-14 16:37:42 +0100

    Added lookup controller

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,7 +22,8 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.6.0"
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.6.0",
+    "uk.gov.hmrc" %% "domain" % "5.0.0"
   )
 
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
