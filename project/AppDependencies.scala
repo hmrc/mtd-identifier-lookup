c850c0c1058cbaaf2eec813c29f6acb8a987a19a
mac grewal <mac.grewal@hotmail.com>
2018-09-18 10:43:09 +0100

    Updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,8 +22,8 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.6.0",
-    "uk.gov.hmrc" %% "domain" % "5.0.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "3.6.0",
+    "uk.gov.hmrc" %% "domain" % "5.2.0",
     "uk.gov.hmrc" %% "play-reactivemongo" % "6.2.0"
   )
 
