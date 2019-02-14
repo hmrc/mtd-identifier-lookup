603c455212f8081049b8568eb73cebce83fd97ab
Jacob Brownlow <35334115+jacobbrownlow@users.noreply.github.com>
2019-02-14 16:41:31 +0000

    revert reactiveMongo change

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -24,7 +24,7 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc" %% "bootstrap-play-25" % "4.8.0",
     "uk.gov.hmrc" %% "domain" % "5.3.0",
-    "uk.gov.hmrc" %% "play-reactivemongo" % "6.4.0"
+    "uk.gov.hmrc" %% "play-reactivemongo" % "6.2.0"
   )
 
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
