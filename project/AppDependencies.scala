330724e0a0530d986100e22c47028690d36484f5
Jacob Brownlow <35334115+jacobbrownlow@users.noreply.github.com>
2019-02-14 16:42:12 +0000

    Library updates (#14)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,8 +22,8 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.2.0",
-    "uk.gov.hmrc" %% "domain" % "5.2.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.8.0",
+    "uk.gov.hmrc" %% "domain" % "5.3.0",
     "uk.gov.hmrc" %% "play-reactivemongo" % "6.2.0"
   )
 
