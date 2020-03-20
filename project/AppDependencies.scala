871c1bc776fb690823ff0a0a2488e4da7ce3ca7c
yang <20066247+yangwong9664@users.noreply.github.com>
2020-03-20 09:28:54 +0000

    MTDSA-5064 uodate dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,9 +22,9 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.3.0",
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.5.0",
     "uk.gov.hmrc" %% "domain" % "5.6.0-play-26",
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.22.0-play-26",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.25.0-play-26",
     compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.4.4" cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % "1.4.4" % Provided cross CrossVersion.full
   )
