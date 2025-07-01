4d2595ff136d1a77c544f1a9c30028566a12ae56
Ahmad Naeem <67148921+AhmadNaeemQA@users.noreply.github.com>
2025-07-01 11:31:45 +0100

    MTDSA-30402 updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -29,7 +29,7 @@ object AppDependencies {
 
   val test: Seq[sbt.ModuleID] = Seq(
     "uk.gov.hmrc"         %% "bootstrap-test-play-30" % bootstrapPlayVersion,
-    "org.scalamock"       %% "scalamock"              % "7.3.2",
+    "org.scalamock"       %% "scalamock"              % "7.3.3",
     "org.scalatestplus"   %% "scalacheck-1-18"        % "3.2.19.0",
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-30" % mongoPlayVersion
   ).map(_ % Test)
