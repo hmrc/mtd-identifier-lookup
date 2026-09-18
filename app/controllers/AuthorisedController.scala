6b3989cbdf8c9d9e8d131887275c9dde29c809ab
“Taiwo-AB” <63714619+Taiwo-AB@users.noreply.github.com>
2026-09-18 10:41:29 +0100

    Add temp log to debug in ET

--- app/controllers/AuthorisedController.scala
+++ app/controllers/AuthorisedController.scala
@@ -22,15 +22,23 @@ import play.api.mvc.*
 import services.EnrolmentsAuthService
 import uk.gov.hmrc.auth.core.authorise.{EmptyPredicate, Predicate}
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import utils.Logging
 
 import scala.concurrent.{ExecutionContext, Future}
 
-abstract class AuthorisedController(cc: ControllerComponents)(implicit ec: ExecutionContext) extends BackendController(cc) {
+abstract class AuthorisedController(cc: ControllerComponents)(implicit ec: ExecutionContext) extends BackendController(cc) with Logging {
 
   val authService: EnrolmentsAuthService
 
   def authorisedAction(predicate: Predicate = EmptyPredicate)(block: Request[AnyContent] => Future[Result]): Action[AnyContent] = Action.async {
     implicit request =>
+
+      request.headers.get("Content-Type") match {
+        case None                                                                   => logger.info("Content-Type header is missing from the request")
+        case Some(contentType) if !contentType.equalsIgnoreCase("application/json") => logger.info(s"Unexpected Content-Type header: $contentType")
+        case _                                                                      =>
+      }
+
       authService.authorised(predicate) flatMap {
         case Right(_)                  => block(request)
         case Left(AuthError(false, _)) => Future.successful(Unauthorized(Json.obj()))
